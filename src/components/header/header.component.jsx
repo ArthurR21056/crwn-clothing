47c45757e0f4d4fca0eed93795f67549eceea99f
Arthur Reeves <Arthur.Reeves@jjwws.com>
2019-12-20 09:16:07 -0600

    added redux and implemented userReducer and userActions to currentUrse

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
+
 import './header.styles.scss';
 
 const Header = ({currentUser}) => (
@@ -26,6 +28,11 @@ const Header = ({currentUser}) => (
             }
         </div>
     </div>
-)
+);
 
-export default Header;
+ const mapStateToProps = state => ({
+     currentUser: state.user.currentUser
+
+ })
+
+export default connect(mapStateToProps)(Header);
